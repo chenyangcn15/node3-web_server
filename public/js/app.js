33070240fb76491b4f71d474c5505580e7424d88
Chen <chenyangcn15@gmail.com>
2019-07-30 19:59:50 +0800

    adding a welcome console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 console.log('Client side javascript file is loaded!')
-
+console.log('restart to learn nodejs')
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
